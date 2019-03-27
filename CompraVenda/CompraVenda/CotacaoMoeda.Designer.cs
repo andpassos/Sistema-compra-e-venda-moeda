e1f77ccaf3bd4d26f1722dc87c038606f196766d
Anderson Passos <and.passos20@gmail.com>
2019-03-26 22:19:39 -0300

    Programa feito em aula

---
+++ CompraVenda/CompraVenda/CotacaoMoeda.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace CompraVenda
+{
+    partial class CotacaoMoeda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbMoeda = new System.Windows.Forms.ComboBox();
+            this.btnCotar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Moeda: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cmbMoeda
+            // 
+            this.cmbMoeda.FormattingEnabled = true;
+            this.cmbMoeda.Items.AddRange(new object[] {
+            "Dólar Americano (venda)",
+            "Dólar Americano (compra)",
+            "Euro (venda)",
+            "Euro (compra)",
+            "Iene (venda)",
+            "Iene (compra)",
+            "Dólar Canadense (venda)",
+            "Dólar Canadense (compra)"});
+            this.cmbMoeda.Location = new System.Drawing.Point(64, 49);
+            this.cmbMoeda.Name = "cmbMoeda";
+            this.cmbMoeda.Size = new System.Drawing.Size(222, 21);
+            this.cmbMoeda.TabIndex = 1;
+            // 
+            // btnCotar
+            // 
+            this.btnCotar.Location = new System.Drawing.Point(344, 49);
+            this.btnCotar.Name = "btnCotar";
+            this.btnCotar.Size = new System.Drawing.Size(75, 23);
+            this.btnCotar.TabIndex = 2;
+            this.btnCotar.Text = "COTAR";
+            this.btnCotar.UseVisualStyleBackColor = true;
+            this.btnCotar.Click += new System.EventHandler(this.btnCotar_Click);
+            // 
+            // CotacaoMoeda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 152);
+            this.Controls.Add(this.btnCotar);
+            this.Controls.Add(this.cmbMoeda);
+            this.Controls.Add(this.label1);
+            this.Name = "CotacaoMoeda";
+            this.Text = "Cotação Moeda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbMoeda;
+        private System.Windows.Forms.Button btnCotar;
+    }
+}
---
+++ CompraVenda/CompraVenda/CotacaoMoeda.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompraVenda
+{
+    public partial class CotacaoMoeda : Form
+    {
+        public CotacaoMoeda()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCotar_Click(object sender, EventArgs e)
+        {
+            int codMoeda = 0;
+
+            if (cmbMoeda.Items[cmbMoeda.SelectedIndex].ToString() == "Dólar Americano (venda)")
+                codMoeda = 1;
+
+            if (cmbMoeda.Items[cmbMoeda.SelectedIndex].ToString() == "Dólar Americano (compra)")
+                codMoeda = 10813;
+
+            if (cmbMoeda.Items[cmbMoeda.SelectedIndex].ToString() == "Euro (venda)")
+                codMoeda = 21619;
+
+            if (cmbMoeda.Items[cmbMoeda.SelectedIndex].ToString() == "Euro (compra)")
+                codMoeda = 21620;
+
+            if (cmbMoeda.Items[cmbMoeda.SelectedIndex].ToString() == "Iene (venda)")
+                codMoeda = 21621;
+
+            if (cmbMoeda.Items[cmbMoeda.SelectedIndex].ToString() == "Iene (compra)")
+                codMoeda = 21622;
+
+            if (cmbMoeda.Items[cmbMoeda.SelectedIndex].ToString() == "Dólar Canadense (venda)")
+                codMoeda = 21635;
+
+            if (cmbMoeda.Items[cmbMoeda.SelectedIndex].ToString() == "Dólar Canadense (compra)")
+                codMoeda = 21636;
+
+            Cotar(codMoeda);
+        }
+
+        private void Cotar(int codMoeda)
+        {
+
+            var service = new Moeda.FachadaWSSGSClient();
+            var cotacao = service.getUltimoValorVO(codMoeda);
+            string valor = (cotacao.ultimoValor.svalor).ToString();
+            MessageBox.Show("R$ " + valor);
+        }
+        /*
+        Dólar Americano (venda)
+        Dólar Americano (compra)
+        Euro (venda)
+        Euro (compra)
+        Iene (venda)
+        Iene (compra)
+        Dólar Canadense (venda)
+        Dólar Canadense (compra)
+
+        1		Dólar (venda)
+        10813	Dólar (compra)
+        21619	Euro (venda)
+        21620	Euro (compra)
+        21621	Iene (venda)
+        21622	Iene (compra)
+        21623	Libra esterlina (venda)
+        21624	Libra esterlina (compra)
+        21625	Franco Suíço (venda)
+        21626	Franco Suíço (compra)
+        21627	Coroa Dinamarquesa (venda)
+        21628	Coroa Dinamarquesa (compra)
+        21629	Coroa Norueguesa (venda)
+        21630	Coroa Norueguesa (compra)
+        21631	Coroa Sueca (venda)
+        21632	Coroa Sueca (compra)
+        21633	Dólar Australiano (venda)
+        21634	Dólar Australiano (compra)
+        21635	Dólar Canadense (venda)
+        21636	Dólar Canadense (compra)
+
+        https://www3.bcb.gov.br/sgspub/JSP/sgsgeral/FachadaWSSGS.wsdl*/
+
+    }
+}
